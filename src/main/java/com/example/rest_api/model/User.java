16b91e849077ac53ca13f3474f9fc4d031caf2e4
MikiKru <michal_kruczkowski@o2.pl>
2020-11-03 16:04:47 +0100

    ex3

--- src/main/java/com/example/rest_api/model/User.java
+++ src/main/java/com/example/rest_api/model/User.java
@@ -1,30 +1,35 @@
 package com.example.rest_api.model;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
+import org.hibernate.annotations.Type;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 // adnotacja ORM Object Relationship Mapping
                  // Utworzona zostaje tableka MySQL o nazwie user i
 @Entity          // kolumnach takich jak pola klasowe
 @Data            // auto-generowanie getters/setters/toString
+@AllArgsConstructor // auto-generacja konstruktora z wszystkimi polami w argymencie
+@NoArgsConstructor  // auto-generacja kontrukotra bezargumentowego
 //@Getter          // auto-generowanie getters
 //@Setter          // auto-generowanie Setters
 //@ToString          // auto-generowanie metody toString()
+@Table(name = "users")
 public class User {
     @Id                                                 // klucz główny
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto inkrementacja dot tylko tabelki
     private int userId;
     private String name;
     private String lastName;
+    @Column(unique = true)
     private String email;
     private String password;
+    @Column(name = "registraton_time")                 // edycja właściwości kolumny
     private LocalDateTime registrationDateTime = LocalDateTime.now();
+    @Type(type = "text")
+    private String description;
     private boolean status = true;
+    @Transient                                          // wykluczenie z mapowania ORM
+    private String secretCode;
 }
