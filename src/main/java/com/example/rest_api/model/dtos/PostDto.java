0349a8a85ad96f0de338f8b17281778e20b48c7c
MikiKru <michal_kruczkowski@o2.pl>
2020-12-08 15:36:30 +0100

    lecture 08.12.2020 v1

--- src/main/java/com/example/rest_api/model/dtos/PostDto.java
+++ src/main/java/com/example/rest_api/model/dtos/PostDto.java
@@ -4,13 +4,26 @@ import com.example.rest_api.model.enums.Category;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 // Klasa do transferu warości pomiędzy warstwą fornt-end i back-end
 // Klasa wspierająca adnotacje dotyczące walidacji
 @AllArgsConstructor
 @Data
 public class PostDto {
+//    @NotNull      // sprawdza czy wartość przekazywanego obiektu nie jest null
+//    @NotEmpty     // sprawdza czy wartośc przekazywanego obiektu nie jest ""
+//    @NotBlank     // kombinacja dwóch powyższych adnotacji
+    @NotBlank(message = "Title must be not null")
+    @Size(min = 5, max = 255, message = "Title must contain at least {min} to {max} characters")
     private String title;
+    @NotBlank(message = "Content must be not null")
+    @Size(min = 255, message = "Content must contain at least {min} characters")
     private String content;
     private Category category;
+    @NotBlank(message = "Author id must be not null")
     private int authorId;
 }
