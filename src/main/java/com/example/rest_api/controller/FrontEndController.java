fcfa44242313ac58b309036cdb4868d007dadbc0
MikiKru <michal_kruczkowski@o2.pl>
2021-01-12 16:39:59 +0100

    lecture 12.01.2021 v8

--- src/main/java/com/example/rest_api/controller/FrontEndController.java
+++ src/main/java/com/example/rest_api/controller/FrontEndController.java
@@ -1,12 +1,16 @@
 package com.example.rest_api.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller     // klasa która mapuje żądania http na metody i zwraca nazwę widoku html
 public class FrontEndController {
     @GetMapping("/")
-    public String home(){
+    public String home(Authentication auth){
+        UserDetails userDetails = (UserDetails) auth.getPrincipal();
+        System.out.println("Logging credentials: " + userDetails);
         return "home";
     }
     @GetMapping("/login")
