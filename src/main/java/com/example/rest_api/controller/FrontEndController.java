032254b3606dca93d2b7dd1cafe9aa4ba22bdba8
MikiKru <michal_kruczkowski@o2.pl>
2021-01-12 16:13:02 +0100

    lecture 12.01.2021 v4

--- src/main/java/com/example/rest_api/controller/FrontEndController.java
+++ src/main/java/com/example/rest_api/controller/FrontEndController.java
@@ -5,8 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller     // klasa która mapuje żądania http na metody i zwraca nazwę widoku html
 public class FrontEndController {
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
     @GetMapping("/login")
     public String login(){
-        return "loginPage";     // widok z resources i bez rozszerzenia html
+        return "loginPage";     // widok z resources/templates i bez rozszerzenia html
     }
 }
