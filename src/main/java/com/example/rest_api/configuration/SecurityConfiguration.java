bdb7968e1e918fd44db37a99bdbbf9466028d3fe
MikiKru <michal_kruczkowski@o2.pl>
2021-01-12 16:26:00 +0100

    lecture 12.01.2021 v5

--- src/main/java/com/example/rest_api/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/rest_api/configuration/SecurityConfiguration.java
@@ -1,9 +1,14 @@
 package com.example.rest_api.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 @Configuration      // klasa o specyficznym znaczeniu implementująca globalne zabezpieczenia w aplikcji
@@ -19,9 +24,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .loginPage("/login")
                         .usernameParameter("email")
                         .passwordParameter("password")
-                        .loginProcessingUrl("login_process")
+                        .loginProcessingUrl("/login_process")
                         .failureUrl("/login?error=true")
                         .defaultSuccessUrl("/");
-
     }
+    @Autowired
+    DataSource dataSource;
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .usersByUsernameQuery("SELECT u.email,u.password,u.status FROM user u WHERE u.email=?")
+                .authoritiesByUsernameQuery(
+                        "SELECT u.email, r.role_name FROM user u JOIN users_to_roles ur ON ur.user_id = u.user_id" +
+                                " JOIN role r ON r.role_id = ur.role_id WHERE u.email=?"
+                )
+                .dataSource(dataSource)
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+
 }
