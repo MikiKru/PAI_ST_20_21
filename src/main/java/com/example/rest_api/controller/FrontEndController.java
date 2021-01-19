94c50935f079079566e30b8699a1d5601b0ea28d
MikiKru <michal_kruczkowski@o2.pl>
2021-01-19 15:51:02 +0100

    lect 19.01.2021 v1

--- src/main/java/com/example/rest_api/controller/FrontEndController.java
+++ src/main/java/com/example/rest_api/controller/FrontEndController.java
@@ -1,14 +1,26 @@
 package com.example.rest_api.controller;
 
 import com.example.rest_api.model.User;
+import com.example.rest_api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDateTime;
 
 @Controller     // klasa która mapuje żądania http na metody i zwraca nazwę widoku html
 public class FrontEndController {
+    private UserService userService;
+    @Autowired
+    public FrontEndController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/")
     public String home(Authentication auth){
         UserDetails userDetails = (UserDetails) auth.getPrincipal();
@@ -22,5 +34,11 @@ public class FrontEndController {
         model.addAttribute("user", new User());
         return "loginPage";     // widok z resources/templates i bez rozszerzenia html
     }
-
+    @PostMapping("/registration")
+    public String registration(@ModelAttribute User user){
+        user.setRegistrationDateTime(LocalDateTime.now());
+        user.setStatus(true);
+        userService.insertUser(user);
+        return "redirect:/login";       // przekierowanie na adres metodą GET
+    }
 }
